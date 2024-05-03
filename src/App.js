800364d045a909c6121ed3399e3374b884f4afc3
Nguyen <thainh@twinger.vn>
2024-05-03 17:46:33 +0700

    chart

--- src/App.js
+++ src/App.js
@@ -1,19 +1,72 @@
-import "./App.css";
-import Comments from "./components/Comment";
-import { Play } from "./components/Play";
+import ReactApexChart from "react-apexcharts";
 
-import axios from "axios";
+import "./App.css";
 
 function App() {
-  console.log({...axios});
+  const configChart = {
+    series: [
+      {
+        name: "Desktops",
+        data: [10, 41, 35, 51, 49, 62, 69, 91, 148],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "line",
+        zoom: {
+          enabled: false,
+        },
+      },
+      dataLabels: {
+        enabled: true,
+        offsetX: 0,
+        offsetY: -8,
+        background: {
+          borderRadius: 9999,
+          padding: 6,
+        },
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      markers: {
+        size: 6,
+        showNullDataPoints: false,
+      },
+      title: {
+        text: "Product Trends by Month",
+        align: "left",
+      },
+      grid: {
+        row: {
+          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
+          opacity: 0.5,
+        },
+      },
+      xaxis: {
+        categories: [
+          "Jan",
+          "Feb",
+          "Mar",
+          "Apr",
+          "May",
+          "Jun",
+          "Jul",
+          "Aug",
+          "Sep",
+        ],
+      },
+    },
+  };
+
   return (
     <body data-theme="light" className="App">
-      <Play />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Comments />
+      <ReactApexChart
+        options={configChart.options}
+        series={configChart.series}
+        height={350}
+      />
     </body>
   );
 }
