89920d052799b5ba9c16dcdfde014f54bd0cc5d0
thainh <nguyenthai9cc@gmail.com>
2024-05-05 09:44:45 +0700

    Revert "chart"
    This reverts commit 800364d045a909c6121ed3399e3374b884f4afc3.

--- src/App.js
+++ src/App.js
@@ -1,82 +1,19 @@
-import ReactApexChart from "react-apexcharts";
-
 import "./App.css";
+import Comments from "./components/Comment";
+import { Play } from "./components/Play";
+
+import axios from "axios";
 
 function App() {
-  const configChart = {
-    series: [
-      {
-        name: "High - 2013",
-        data: [28, 29, 33, 36, 32, 32, 33, 29, 34, 28, 26, 25],
-      },
-      {
-        name: "Low - 2013",
-        data: [12, 11, 14, 18, 17, 13, 13, 15, 12, 16, 14, 15],
-      },
-    ],
-    options: {
-      chart: {
-        type: "line",
-        zoom: {
-          enabled: false,
-        },
-      },
-      dataLabels: {
-        enabled: true,
-        offsetX: 0,
-        offsetY: -6,
-        background: false,
-      },
-      stroke: {
-        curve: "smooth",
-      },
-      markers: {
-        size: 6,
-        showNullDataPoints: false,
-      },
-      title: {
-        text: "Product Trends by Month",
-        align: "left",
-      },
-      colors: ["#77B6EA", "#545454"],
-      grid: {
-        row: {
-          colors: ["#f3f3f3", "transparent"], // takes an array which will be repeated on columns
-          opacity: 0.5,
-        },
-        padding: {
-          left: 18,
-          right: 18,
-        },
-      },
-      xaxis: {
-        categories: [
-          "Jan",
-          "Feb",
-          "Mar",
-          "Apr",
-          "May",
-          "Jun",
-          "Jul",
-          "Aug",
-          "Sep",
-          "Otc",
-          "Nov",
-          "Dec",
-        ],
-      },
-    },
-  };
-
+  console.log({...axios});
   return (
-    <body data-theme="light" className="app-container">
-      <div className="app-wrapper">
-        <ReactApexChart
-          options={configChart.options}
-          series={configChart.series}
-          height={350}
-        />
-      </div>
+    <body data-theme="light" className="App">
+      <Play />
+      <br />
+      <br />
+      <br />
+      <br />
+      <Comments />
     </body>
   );
 }
